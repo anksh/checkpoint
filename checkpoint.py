8d82129a017e5f52a76191c5e5e3e35bcd4de394
Ankit Shah <ank.h.shah@gmail.com>
2016-04-27 16:44:57 -0700

    More stubs

--- checkpoint.py
+++ checkpoint.py
@@ -3,26 +3,51 @@
 import os, sys
 
 
-def get_directory(*args):
+def _get_current_directory():
     return os.getcwd()
 
+def add_checkpoint():
+    # option 1: read file
+    # find next unused entry
+    # write to back of file
+    # option 2: keep a global count
+    # update count and write to back of file
+    pass
 
-def choose_function():
-#   look at sys args and decide which function needs to be called
-#   if args are valid, return the chosen function
-#   else throw/return None
-    if(len(sys.argv) != 1):
-        raise RuntimeError()
-    return get_directory
+def go_to_checkpoint(target):
+    # find the checkpoint
+    # cd to that checkpoint
+    pass
+
+def delete_checkpoint(victim):
+    # read file
+    # find checkpoint
+    # remove it
+    # rewrite file
+    pass
+
+def erase_all_checkpoints():
+    # as simple as clearing the file
+    pass
+
+def list_checkpoints():
+    # print out all checkpoints in a readable manner
+    pass
+
 
 def main():
-    try:
-        chosen_function = choose_function()
-    except RuntimeError:
-        print("Invalid input! Please use the -h flag for more info")
-        return
+    # look at sys args and decide which function needs to be called
+    # if args are valid, return the chosen function
+    # else throw/return None
 
-    print(chosen_function(sys.argv[1:]))
+    # different options:
+    #     n: adds a new checkpoint
+    #     <int>: goes to that directory (if it exists)
+    #     d <int>: deletes a specific checkpoint - should prompt the user before doing so
+    #     e: erases all checkpoints - should prompt the user before doing so
+    #     l: list all checkpoints
+    pass
+
 
 if __name__ == '__main__':
     main()
